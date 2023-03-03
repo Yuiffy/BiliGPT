a0bd2057f34a7fa04d54d527ca71a70d1fd65fa2
Yuiffy <me@yuiffy.com>
2023-03-03 19:49:52 +0800

    feat: 增加b站cookie。b站对没cookie的用户不返回字幕了

--- utils/3rd/bilibili.ts
+++ utils/3rd/bilibili.ts
@@ -1,17 +1,29 @@
 import pRetry from "p-retry";
 
+var myHeaders = new Headers();
+myHeaders.append("authority", "api.bilibili.com");
+myHeaders.append("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+myHeaders.append("accept-language", "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7,zh-TW;q=0.6,ja;q=0.5");
+myHeaders.append("cache-control", "max-age=0");
+myHeaders.append("cookie", process.env.BILIBILI_COOKIE);
+myHeaders.append("sec-ch-ua", "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"");
+myHeaders.append("sec-ch-ua-mobile", "?0");
+myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
+myHeaders.append("sec-fetch-dest", "document");
+myHeaders.append("sec-fetch-mode", "navigate");
+myHeaders.append("sec-fetch-site", "none");
+myHeaders.append("sec-fetch-user", "?1");
+myHeaders.append("upgrade-insecure-requests", "1");
+myHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
 const run = async (bvId: string) => {
   const requestUrl = `https://api.bilibili.com/x/web-interface/view?bvid=${bvId}`;
   console.log(`fetch`, requestUrl);
   const response = await fetch(requestUrl, {
     method: "GET",
+    headers: myHeaders,
   });
   const json = await response.json();
-  const subtitleList = json.data?.subtitle?.list;
-  if (!subtitleList || subtitleList?.length < 1) {
-    throw new Error("no subtitle");
-  }
-
+  // 多run几次也拿不到字幕，所以这里不判断字幕了，在外面判断。
   return json;
 };
 
@@ -24,6 +36,7 @@ export async function fetchSubtitle(bvId: string) {
     },
     retries: 2,
   });
+
   // @ts-ignore
   const title = res.data?.title;
   const subtitleList = res.data?.subtitle?.list;
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,3 @@
 export const CHECKOUT_URL =
   "https://jimmylv.lemonsqueezy.com/checkout/buy/1ddaaa34-05eb-4db6-8c48-3ac0faed1b94";
-export const RATE_LIMIT_COUNT = 7;
+export const RATE_LIMIT_COUNT = 12345;
